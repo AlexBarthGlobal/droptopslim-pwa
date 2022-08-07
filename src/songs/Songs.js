d9c0d38849275135a680621f0aade1823167d465
Alex Barth <alexbarthglobal@gmail.com>
2022-08-07 13:38:58 -0400

    Cleaned up directory structure, renamed images and video files, added new songs

--- src/songs/Songs.js
+++ src/songs/Songs.js
@@ -6,15 +6,22 @@ import './videos.css';
 import Video from './Video'
 
 // Song images
-import lastday from './artwork/lastday.jpg';
-import badguy from './artwork/badguy.jpg';
-import guccifromharlem from './artwork/guccifromharlem.jpg';
-import non from './artwork/non.jpg'
-import stillFeelMe from './artwork/stillfeelme.jpg'
+import all_in from './artwork/songs/all_in.jpg';
+import bad_guy from './artwork/songs/bad_guy.jpg';
+import chanel_and_dior from './artwork/songs/chanel_and_dior.jpg';
+import convertible_day from './artwork/songs/convertible_day.jpg';
+import destiny from './artwork/songs/destiny.jpg';
+import growing_pain from './artwork/songs/growing_pain.jpg';
+import gucci_from_harlem from './artwork/songs/gucci_from_harlem.jpg';
+import hottest_n_tha_game from './artwork/songs/hottest_n_tha_game.jpg';
+import last_day from './artwork/songs/last_day.jpg';
+import non from './artwork/songs/non.jpg'
+import somewhere_to_be from './artwork/songs/somewhere_to_be.jpg';
+import still_feel_me from './artwork/songs/still_feel_me.jpg'
+import unstoppable from './artwork/songs/unstoppable.jpg';
 
-// Videos images
-// import chanelAndDior from './artwork/chanel-and-dior-vid.jpeg'
-import chanelAndDior from './artwork/chanel-and-dior-vid-3a.png'
+// Video images
+// import chanel_and_dior_vid from './artwork/videos/chanel_and_dior_vid_3a.png'
 
 
 
@@ -27,22 +34,25 @@ const Songs = () => {
   
     return (
         <div className='songs-screen-wrapper'>
-            {/* <div id='bluwhl-songs'>
-                <BlueWhlSongs />
-            </div> */}
-            <div className='videos-container'>
+            {/* <div className='videos-container'>
                 <Video link={'https://m.youtube.com/watch?v=jgOXd0K3r_E'} img={chanelAndDior} />
-            </div>
+            </div> */}
             <div className='songs-container'>
-                <Song link={'https://ffm.to/stillfeelmefreestyle'} img={stillFeelMe} />
-                <Song link={'https://ffm.to/droptoplastday.OWE'} img={lastday} />
-                <Song link={'https://ffm.to/droptopbadguy.OWE'} img={badguy} />
-                <Song link={'https://ffm.to/guccifromharlem.OWE'} img={guccifromharlem} />
+                <Song link={'https://ffm.to/chanelanddior'} img={chanel_and_dior} />
+                <Song link={'https://ffm.to/stillfeelmefreestyle'} img={still_feel_me} />
+                <Song link={'https://ffm.to/droptoplastday.OWE'} img={last_day} />
+                <Song link={'https://ffm.to/guccifromharlem.OWE'} img={gucci_from_harlem} />
+                <Song link={'https://ffm.to/droptopbadguy.OWE'} img={bad_guy} />
                 <Song link={'https://ffm.to/nonigga.OWE'} img={non} />
+                <Song link={'https://ffm.to/droptopallin'} img={all_in} />
+                <Song link={'https://ffm.to/hottestnthagame'} img={hottest_n_tha_game} />
+                <Song link={'https://ffm.to/growingpain'} img={growing_pain} />
+                <Song link={'https://ffm.to/convertibleday'} img={convertible_day} />
+                <Song link={'https://ffm.to/droptopdestiny'} img={destiny} />
+                <Song link={'https://ffm.to/somewheretobe'} img={somewhere_to_be} />
+                <Song link={'https://ffm.to/droptopunstoppable'} img={unstoppable} />
             </div>
-            {/* <div id='testdiv'></div> */}
         </div>
-        
     )
 }
 
